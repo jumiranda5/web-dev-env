de9c4ab264cd8cfe9b0ab83038f1a6d9b5283dc2
Juliana <jumiranda0501@gmail.com>
2023-12-23 13:16:34 -0300

    Set config for dev and prod

--- webpack.common.js
+++ webpack.common.js
@@ -2,11 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: {
       index: './src/index.js',
     },
-    devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/index.html',
